4b470f7ac01d11b34ee2ebb51c324f37a69072f3
Kyle <45010713+kylekrny@users.noreply.github.com>
2025-01-01 23:03:59 -0500

    update title

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className='container mx-auto px-4 sm:px-6 lg:px-8 h-full flex flex-col justify-center text-center'>
       <h1 className='text-8xl font-light'>
