f3e349c84ebf484c547ba59fdcee4a9cca9a784e
Kyle <45010713+kylekrny@users.noreply.github.com>
2025-01-01 22:52:59 -0500

    setup basic 404 page

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import './index.css';
 import App from './App.tsx';
+import NotFound from './pages/NotFound.tsx';
 
 const root = document.getElementById('root')!;
 
@@ -11,6 +12,8 @@ createRoot(root).render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />} />
+
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
