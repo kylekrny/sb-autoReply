05928647cf5710a2903386bc04a38c1cae2fde05
Kyle <45010713+kylekrny@users.noreply.github.com>
2025-03-04 15:30:10 -0500

    setup dialog component

---
+++ src/components/dialog/dialog-actions.tsx
@@ -0,0 +1,16 @@
+import clsx from 'clsx';
+
+export function DialogActions({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<'div'>) {
+  return (
+    <div
+      {...props}
+      className={clsx(
+        className,
+        'mt-8 flex flex-col-reverse items-center justify-end gap-3 *:w-full sm:flex-row sm:*:w-auto'
+      )}
+    />
+  );
+}
