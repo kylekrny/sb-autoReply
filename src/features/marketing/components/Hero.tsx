903a96ed4072b8f7691bd20f7fc8b3f3856e1295
Kyle <45010713+kylekrny@users.noreply.github.com>
2025-03-06 22:13:26 -0500

    basic landing page

---
+++ src/features/marketing/components/Hero.tsx
@@ -0,0 +1,114 @@
+import { ChatBubbleOvalLeftIcon } from '@heroicons/react/24/outline';
+
+export default function Hero() {
+  return (
+    <div className='bg-gray-800 w-full h-full'>
+      <div className='flex text-gray-300 pt-5 pl-5'>
+        <ChatBubbleOvalLeftIcon className='w-6 h-6 mr-2' />
+        <span>SB AutoReply</span>
+      </div>
+      <div className='relative isolate overflow-hidden pt-14 w-full h-full'>
+        <svg
+          aria-hidden='true'
+          className='absolute inset-0 -z-10 size-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]'
+        >
+          <defs>
+            <pattern
+              x='50%'
+              y={-1}
+              id='983e3e4c-de6d-4c3f-8d64-b9761d1534cc'
+              width={200}
+              height={200}
+              patternUnits='userSpaceOnUse'
+            >
+              <path d='M.5 200V.5H200' fill='none' />
+            </pattern>
+          </defs>
+          <svg x='50%' y={-1} className='overflow-visible fill-gray-800/20'>
+            <path
+              d='M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z'
+              strokeWidth={0}
+            />
+          </svg>
+          <rect
+            fill='url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)'
+            width='100%'
+            height='100%'
+            strokeWidth={0}
+          />
+        </svg>
+        <div
+          aria-hidden='true'
+          className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
+        >
+          <div
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+            className='relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
+          />
+        </div>
+        <div className='mx-auto max-w-7xl px-6 lg:px-8 h-full'>
+          <div className='mx-auto max-w-2xl py-32 sm:py-48 lg:py-56 h-full flex flex-col justify-center'>
+            <div className='text-center'>
+              <h1 className='text-4xl font-semibold tracking-tight text-balance text-white sm:text-5xl'>
+                Auto-reply to missed calls. Keep business{' '}
+                <span className='relative whitespace-nowrap text-orange-500'>
+                  <svg
+                    aria-hidden='true'
+                    viewBox='0 0 418 42'
+                    className='absolute top-2/3 left-0 h-[0.58em] w-full fill-orange-500/70'
+                    preserveAspectRatio='none'
+                  >
+                    <path d='M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z' />
+                  </svg>
+                  <span className='relative'>moving.</span>
+                </span>
+              </h1>
+              <p className='mt-8 text-lg font-medium text-pretty text-gray-300 sm:text-xl/8'>
+                SB AutoReply automatically texts back missed callers so you
+                never lose a lead. Simple setup, no number porting required.
+              </p>
+              <div className='mt-10 flex flex-col items-center justify-center gap-x-6'>
+                <h6>Join the waitlist for early access!</h6>
+                <form className='mx-auto mt-6 flex max-w-md gap-x-4'>
+                  <label htmlFor='email-address' className='sr-only'>
+                    Email address
+                  </label>
+                  <input
+                    id='email-address'
+                    name='email'
+                    type='email'
+                    required
+                    placeholder='Enter your email'
+                    autoComplete='email'
+                    className='min-w-0 flex-auto rounded-md bg-white/5 px-3.5 py-2 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-white sm:text-sm/6'
+                  />
+                  <button
+                    type='submit'
+                    className='flex-none rounded-md bg-orange-500 px-5 py-3.5 text-sm font-semibold text-gray-900 shadow-xs hover:bg-orange-100 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white'
+                  >
+                    Notify me
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          aria-hidden='true'
+          className='absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]'
+        >
+          <div
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+            className='relative left-[calc(50%+3rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]'
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
