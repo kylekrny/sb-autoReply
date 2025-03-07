e72072c4738c0710f71d2edb4c054abd6b74fa2c
Kyle <45010713+kylekrny@users.noreply.github.com>
2025-03-07 09:03:45 -0500

    clean up tailwind dependencies

--- src/features/marketing/components/Hero.tsx
+++ src/features/marketing/components/Hero.tsx
@@ -68,18 +68,6 @@ export default function Hero() {
             strokeWidth={0}
           />
         </svg>
-        <div
-          aria-hidden='true'
-          className='absolute inset-x-0 -top-40 -z-10 transform-gpu blur-3xl sm:-top-80'
-        >
-          <div
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-            className='relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
-          />
-        </div>
         <div className='mx-auto max-w-7xl h-full'>
           <div className='mx-auto max-w-2xl h-full flex flex-col justify-center'>
             <div className='text-center'>
